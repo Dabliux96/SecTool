8ace5b65a1e01e6c03e3b94e4cbd0c6d9d89a411
Dabliux96 <gtodaro96@hotmail.com>
2025-07-18 23:07:40 -0400

    Update on scan_ports()

--- network_utils.py
+++ network_utils.py
@@ -21,11 +21,6 @@ def get_own_ip_private():
 def get_own_ip_public():
     public_ip = re.get('https://api.ipify.org?format=json').json()['ip']
     return public_ip
-    #user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36'
-    #headers = {'user-agent': user_agent}
-    #req = re.get('https://whatismyipaddress.com/', headers=headers, allow_redirects=True).text
-    #req = re.get('https://api.ipify.org?format=json').text
-    #public_ip = req.split(':"')[1].strip().replace('"}', '')
 
 def get_target_ip():
     target_IPs = input("Target IP(s): ").replace(' ', '').split(',')
@@ -49,21 +44,6 @@ def get_target_ports():
             return target_ports
 
 
-
-
-# def get_target_ports():
-#     valid_ports = range(1,65536)
-
-#     while True:
-#         target_ports = input("Target port(s): ").replace(' ', '').split(',')
-
-#         for target_port in target_ports:
-#             target_port = int(target_port)
-#             if not target_port in valid_ports:
-#                 print(f'\n[!] ERROR: {target_port} is not a valid port. Try again.')
-#                 break
-#         else: # tied to for loop, ensures that the return only runs if for loop was not broken. As if "for [...] if not break [...]"
-#             return target_ports
 
 def new_socket():
     return socket.socket(socket.AF_INET, socket.SOCK_STREAM)
--- scan_ports.py
+++ scan_ports.py
@@ -5,24 +5,29 @@ def scan_ports():
 
     while True:
         target_IPs = network_utils.get_target_ip()
+        
         if not len(target_IPs) == 1:
             print('[ERROR] Only 1 IP per scan allowed!')
             continue
+
         target_ports = network_utils.get_target_ports()
-        print(target_ports)
-        s = network_utils.new_socket()
+        
+        
+        print('\n')
 
         for target_port in target_ports:
-            for target_IP in target_IPs:
-                
-                s_connection = s.connect_ex((target_IP, target_port))
+        
+            s = network_utils.new_socket()
+            s_connection = s.connect_ex((target_IPs[0], target_port))
 
-                if s_connection == 0:
-                    print(f'{target_port}: Open')
-                    scanned_ports.append(target_port)
-                s.close()
-                break
-            print(scanned_ports)
-            return scanned_ports
+            if s_connection == 0:
+                print(f'{target_port}: Open')
+                scanned_ports.append(target_port)
+            s.close()
+
+        
+        print(f'\nOpen Ports: {(''.join(str(scanned_ports)))}')
+        return scanned_ports
+        
 
 scan_ports()
